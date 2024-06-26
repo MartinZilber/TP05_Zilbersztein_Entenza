f2bac553599ba6450416818cd026b041babdd381
MARTIN IOEL ZILBERSZTEIN <48520155@ort>
2024-06-26 08:35:04 -0300

    Transición de pasos en CSS

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function mostrarpaso(paso)
+{
+    $(".paso").hide();
+    $("#paso"+paso).fadeIn();
+}
