05286fc6016c9164c0113a37528f739f5c25d5e0
MARTIN IOEL ZILBERSZTEIN <48520155@ort>
2024-06-26 07:52:55 -0300

    Primera fase de clase estática

---
+++ Models/Escape.cs
@@ -0,0 +1,23 @@
+class Escape
+{
+    static private string[] incognitasSalas { get; set; } = { "bla,", "hola" };
+    static private int estadoJuego{get;set;} = 1;
+
+    private static void InicializarJuego(){
+
+    }
+    public static int GetEstadoJuego(){
+        return 0;
+    }
+    public static bool ResolverSala(int Sala, string Incognita){
+        bool esCorrecto = false;
+        if (Incognita == incognitasSalas[Sala]){
+            estadoJuego++;
+            esCorrecto = true;
+        }
+        else if (incognitasSalas.Length == 0){
+            InicializarJuego();
+        }
+        return esCorrecto;
+    }
+}
