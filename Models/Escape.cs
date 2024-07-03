7fbcf33f38173fbf7607c7133c43ad18d8d93823
MARTIN IOEL ZILBERSZTEIN <48520155@ort>
2024-07-03 09:28:43 -0300

    Cambio de sala arreglado

--- Models/Escape.cs
+++ Models/Escape.cs
@@ -1,18 +1,21 @@
 class Escape
 {
-    static public string[] incognitasSalas { get; set; } = { "0", "hola" };
-    static public int estadoJuego{get;set;} = 1;
-    static public int contadorIntentos{get;set;} = 0;
+    static public string[] incognitasSalas { get; set; } = { "0", "doce", "piratas" };
+    static public int estadoJuego { get; set; } = 1;
+    static public int contadorIntentos { get; set; } = 0;
 
-    private static void InicializarJuego(){
-        
+    private static void InicializarJuego()
+    {
     }
-    public static int GetEstadoJuego(){
+    public static int GetEstadoJuego()
+    {
         return estadoJuego;
     }
-    public static bool ResolverSala(int Sala, string Incognita){
+    public static bool ResolverSala(int Sala, string Incognita)
+    {
         bool esCorrecto = false;
-        if (Incognita == incognitasSalas[Sala]){
+        if (Incognita == incognitasSalas[estadoJuego-1])
+        {
             estadoJuego++;
             esCorrecto = true;
         }
