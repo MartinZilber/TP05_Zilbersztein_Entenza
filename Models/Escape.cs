1fdce77946ec6dbe0b65a38b650f6ec9a968e7a4
Martín <martin.zilber.2008@gmail.com>
2024-07-24 12:31:49 -0300

    Habitación 6 - Códigos QR

--- Models/Escape.cs
+++ Models/Escape.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures.Buffers;
 class Escape
 {
 
-    static private string[] incognitasSalas { get; set; } = { "0", "cartel", "izquierda", "ramo", "almohadon", "rojo", "piratas", "para elisa", "", "fuego" };
+    static private string[] incognitasSalas { get; set; } = { "0", "cartel", "izquierda", "ramo", "almohadon", "infoesgenial", "piratas", "para elisa", "", "fuego" };
 
     static private int estadoJuego { get; set; } = 1;
     static private int contadorIntentos { get; set; } = 1;
